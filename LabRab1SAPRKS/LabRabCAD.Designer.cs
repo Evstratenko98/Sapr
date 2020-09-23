e097dbc896bca372a8c12d382029c3787bf6d4d2
Evstratenko98 <evstratenko.sv98@gmail.com>
2020-09-23 16:36:35 +0400

    C#

---
+++ LabRab1SAPRKS/LabRabCAD.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace LabRab1SAPRKS
+{
+    partial class LabRabCAD
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LabRabCAD));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.DeleteObjectToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.CreateTreeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CreateTreeDriveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddObjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(209, 426);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            this.treeView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.DeleteObjectToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(146, 26);
+            // 
+            // DeleteObjectToolStripMenuItem1
+            // 
+            this.DeleteObjectToolStripMenuItem1.Name = "DeleteObjectToolStripMenuItem1";
+            this.DeleteObjectToolStripMenuItem1.Size = new System.Drawing.Size(145, 22);
+            this.DeleteObjectToolStripMenuItem1.Text = "Delete Object";
+            this.DeleteObjectToolStripMenuItem1.Click += new System.EventHandler(this.DeleteObjectToolStripMenuItem_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "doc.bmp");
+            this.imageList1.Images.SetKeyName(1, "pdf.bmp");
+            this.imageList1.Images.SetKeyName(2, "txt.bmp");
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CreateTreeToolStripMenuItem,
+            this.CreateTreeDriveToolStripMenuItem,
+            this.AddObjectToolStripMenuItem,
+            this.ExitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // CreateTreeToolStripMenuItem
+            // 
+            this.CreateTreeToolStripMenuItem.Name = "CreateTreeToolStripMenuItem";
+            this.CreateTreeToolStripMenuItem.Size = new System.Drawing.Size(145, 20);
+            this.CreateTreeToolStripMenuItem.Text = "Create Tree (Programm)";
+            this.CreateTreeToolStripMenuItem.Click += new System.EventHandler(this.CreateTreeToolStripMenuItem_Click);
+            // 
+            // CreateTreeDriveToolStripMenuItem
+            // 
+            this.CreateTreeDriveToolStripMenuItem.Name = "CreateTreeDriveToolStripMenuItem";
+            this.CreateTreeDriveToolStripMenuItem.Size = new System.Drawing.Size(115, 20);
+            this.CreateTreeDriveToolStripMenuItem.Text = "Create Tree (Drive)";
+            this.CreateTreeDriveToolStripMenuItem.Click += new System.EventHandler(this.CreateTreeDriveToolStripMenuItem_Click);
+            // 
+            // AddObjectToolStripMenuItem
+            // 
+            this.AddObjectToolStripMenuItem.Name = "AddObjectToolStripMenuItem";
+            this.AddObjectToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.AddObjectToolStripMenuItem.Text = "Add Object";
+            this.AddObjectToolStripMenuItem.Click += new System.EventHandler(this.AddObjectToolStripMenuItem_Click);
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("ExitToolStripMenuItem.Image")));
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(28, 20);
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(212, 24);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(588, 426);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(209, 24);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 426);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // LabRabCAD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "LabRabCAD";
+            this.Text = "LabRabCAD";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CreateTreeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CreateTreeDriveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AddObjectToolStripMenuItem;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem DeleteObjectToolStripMenuItem1;
+    }
+}
+
---
+++ LabRab1SAPRKS/LabRabCAD.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LabRab1SAPRKS
+{
+    public partial class LabRabCAD : Form
+    {
+        public LabRabCAD()
+        {
+            InitializeComponent();
+            CreateTreeDriveToolStripMenuItem.Enabled = false;
+        }
+
+        int n=2;
+        int node = 0; //Счетчик узлов
+        int number = 0;
+        List<int> numbers = new List<int> { 0, 1 };
+
+        TreeNode mySelectedNode;
+
+        public string testdir = @"G:\List\";
+        private void CreateIfMissing(string path)
+        {
+            bool folderExist = Directory.Exists(path);
+            if (!folderExist)
+                Directory.CreateDirectory(path);
+        }
+
+        private void PrintRecursive(TreeNode treeNode)
+        {
+            System.Diagnostics.Debug.WriteLine(treeNode.Text); //печать одного узла
+            //MessageBox.Show(treeNode.FullPath);
+            //просматриваем все родительские вершины, создаем отдельный узел на диске
+            CreateIfMissing(testdir + treeNode.FullPath); 
+
+            //печать каждого узла tn рекурсивно
+            foreach (TreeNode tn in treeNode.Nodes)
+            {
+                PrintRecursive(tn);
+            }
+        }
+
+        private void CallRecursive(TreeView treeView)
+        {
+            TreeNodeCollection nodes = treeView.Nodes;
+            foreach (TreeNode n in nodes)
+            {
+                PrintRecursive(n);
+            }
+            MessageBox.Show("Done!");
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CreateNewTree()
+        {
+            treeView1.Nodes.Clear();
+
+            treeView1.Nodes.Add("1. Исходно-разрешительная документация (ИРД)");
+            treeView1.Nodes.Add("2. Инженерные изыскания (ИИ)");
+            treeView1.Nodes.Add("3. Проектная документация стадии П (ПД)");
+            treeView1.Nodes.Add("4. Проектная документация стадии Р (РД)");
+            treeView1.Nodes.Add("5. Коммерческая документация (КД)");
+
+            foreach (int n in numbers)
+            {
+                int k = n + 1;
+                treeView1.Nodes[0].Nodes.Add("1." + k + " Объект " + k);
+                treeView1.Nodes[0].Nodes[node].Nodes.Add("1." + k + ".1 Документы на ЗУ");
+                treeView1.Nodes[0].Nodes[node].Nodes.Add("1." + k + ".2 Градостроительный план ЗУ");
+                treeView1.Nodes[0].Nodes[node].Nodes.Add("1." + k + ".3 Разрешение на строительство");
+                treeView1.Nodes[0].Nodes[node].Nodes.Add("1." + k + ".4 Технические условия");
+                treeView1.Nodes[0].Nodes[node].Nodes.Add("1." + k + ".5 Проектная декларация");
+
+                treeView1.Nodes[1].Nodes.Add("2." + k + " Объект " + k);
+                treeView1.Nodes[1].Nodes[node].Nodes.Add("2." + k + ".1 Инженерно-геодезические изыскания");
+                treeView1.Nodes[1].Nodes[node].Nodes.Add("2." + k + ".2 Инженерно-геологические изыскания");
+                treeView1.Nodes[1].Nodes[node].Nodes.Add("2." + k + ".3 Инженерно-экологические изыскания");
+
+                treeView1.Nodes[2].Nodes.Add("3." + k + " Объект " + k);
+                treeView1.Nodes[2].Nodes[node].Nodes.Add("3." + k + ".1 Пояснительная записка");
+                treeView1.Nodes[2].Nodes[node].Nodes.Add("3." + k + ".2 Градостроительный план ЗУ");
+                treeView1.Nodes[2].Nodes[node].Nodes.Add("3." + k + ".3 Архитектурные решения");
+                treeView1.Nodes[2].Nodes[node].Nodes.Add("3." + k + ".4 Конструктивные решения");
+
+                treeView1.Nodes[3].Nodes.Add("4." + k + " Объект " + k);
+                treeView1.Nodes[3].Nodes[node].Nodes.Add("4." + k + ".1 Генеральный план");
+                treeView1.Nodes[3].Nodes[node].Nodes.Add("4." + k + ".2 Архитектурно-строительные решения");
+                treeView1.Nodes[3].Nodes[node].Nodes.Add("4." + k + ".3 Архитектурные решения");
+                treeView1.Nodes[3].Nodes[node].Nodes.Add("4." + k + ".4 Конструкции железобетонные");
+
+                treeView1.Nodes[4].Nodes.Add("5." + k + " Объект " + k);
+                treeView1.Nodes[4].Nodes[node].Nodes.Add("5." + k + ".1 Поэтажные планы");
+                treeView1.Nodes[4].Nodes[node].Nodes.Add("5." + k + ".2 Прайсы");
+                treeView1.Nodes[4].Nodes[node].Nodes.Add("5." + k + ".3 Брошюры");
+
+                node++;
+                number++;
+            }
+
+            treeView1.ExpandAll(); //разворачивает дерево
+            treeView1.HotTracking = true; // подсвечивает текст при наведении мышкой
+            CreateTreeDriveToolStripMenuItem.Enabled = true;
+            node = 0; // Очищает счетчик узлов
+        }
+
+        private void CreateTreeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateNewTree();
+        }
+
+        private void CreateTreeDriveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1.ExpandAll();
+            CallRecursive(treeView1);
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            try
+            {
+                listView1.Items.Clear();
+
+                foreach (string file in Directory.GetFiles(testdir + treeView1.SelectedNode.FullPath))
+                {
+                    string fileName = file.Replace((testdir + treeView1.SelectedNode.FullPath + @"\"), String.Empty);
+                    ListViewItem lvi = new ListViewItem(fileName);
+                    lvi.Name = fileName;
+                    string filenameExtension = Path.GetExtension(file.ToLower());
+
+                    switch (filenameExtension)
+                    {
+                        case ".doc":
+                            {
+                                lvi.ImageIndex = 0;
+                                break;
+                            }
+
+                        case ".docx":
+                            {
+                                lvi.ImageIndex = 0;
+                                break;
+                            }
+
+                        case ".pdf":
+                            {
+                                lvi.ImageIndex = 1;
+                                break;
+                            }
+
+                        default:
+                            {
+                                lvi.ImageIndex = 2;
+                                break;
+                            }
+                    }
+
+                    listView1.Items.Add(lvi);
+                }
+            }
+
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp + " Ошииииииибочка");
+            }
+        }
+
+        private void AddObjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            numbers.Add(n);
+            CreateNewTree();
+            n++;
+        }
+
+        private void treeView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mySelectedNode = treeView1.GetNodeAt(e.X, e.Y);
+        }
+
+        private void DeleteObjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (mySelectedNode.Nodes != null && mySelectedNode.Parent != null)
+            {
+                treeView1.SelectedNode = mySelectedNode;
+                const string message = "Are you sure that you would like to delete this object?";
+                const string caption = "Delete Object";
+                var result = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    string nodeStr = mySelectedNode.Text;
+                    char theLastCharacterOfNodeStr = nodeStr[nodeStr.Length - 1];
+                    int bar = theLastCharacterOfNodeStr - '0';
+                    numbers.Remove(bar - 1);
+                    node = 0;
+                    CreateNewTree();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Only objects can be deleted!");
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            listView1.HotTracking = true;
+            ListViewItem myFile = listView1.GetItemAt(e.X, e.Y);
+
+
+            String fullPath = testdir + treeView1.SelectedNode.FullPath + @"\" + myFile.Name;
+            System.Diagnostics.Process.Start(fullPath);
+        }
+    }
+}
